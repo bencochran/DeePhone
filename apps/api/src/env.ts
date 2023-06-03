1f5af90823073e867fb803af407e4cd9f70798a3
Ben Cochran <bcochran@gmail.com>
2023-06-03 11:12:58 -0700

    Fix env in tests

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -1,37 +1,40 @@
 import logger from './logger'
 
-if (!process.env.AWS_REGION) {
+// When testing, let the `undefined`s flow
+const tests = process.env.NODE_ENV === 'test';
+
+if (!tests && !process.env.AWS_REGION) {
   logger.error('Missing AWS_REGION');
   process.exit(1);
 }
-export const AWS_REGION = process.env.AWS_REGION;
+export const AWS_REGION = process.env.AWS_REGION!;
 
-if (!process.env.S3_BUCKET_NAME) {
+if (!tests && !process.env.S3_BUCKET_NAME) {
   logger.error('Missing S3_BUCKET_NAME');
   process.exit(1);
 }
-export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME!;
 
-if (!process.env.S3_BUCKET_BASE_URL) {
+if (!tests && !process.env.S3_BUCKET_BASE_URL) {
   logger.error('Missing S3_BUCKET_BASE_URL');
   process.exit(1);
 }
-export const S3_BUCKET_BASE_URL = process.env.S3_BUCKET_BASE_URL;
+export const S3_BUCKET_BASE_URL = process.env.S3_BUCKET_BASE_URL!;
 
-if (!process.env.AWS_ACCESS_KEY_ID) {
+if (!tests && !process.env.AWS_ACCESS_KEY_ID) {
   logger.error('Missing AWS_ACCESS_KEY_ID');
   process.exit(1);
 }
-export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID!;
 
-if (!process.env.AWS_SECRET_ACCESS_KEY) {
+if (!tests && !process.env.AWS_SECRET_ACCESS_KEY) {
   logger.error('Missing AWS_SECRET_ACCESS_KEY');
   process.exit(1);
 }
-export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY!;
 
-if (!process.env.TWILIO_AUTH_TOKEN) {
+if (!tests && !process.env.TWILIO_AUTH_TOKEN) {
   logger.error('Missing TWILIO_AUTH_TOKEN');
   process.exit(1);
 }
-export const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+export const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN!;
