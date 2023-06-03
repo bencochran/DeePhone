23cc94fa0054f60f5af59da16d66582f360fcc9d
Ben Cochran <bcochran@gmail.com>
2023-06-02 18:09:08 -0700

    Slightly better responsive design

--- apps/web/src/components/Page.tsx
+++ apps/web/src/components/Page.tsx
@@ -6,7 +6,7 @@ export interface PageProps {
 
 export const Page: React.FC<React.PropsWithChildren<PageProps>> = ({ title, children }) => {
   return (
-    <div className='mx-8 my-6'>
+    <div className='mx-4 sm:mx-8 my-6'>
       <div className='mb-4'>
         {typeof title === 'string' ? (
           <h2 className='text-slate-900 dark:text-slate-300 font-medium text-3xl mb-4'>
@@ -14,7 +14,7 @@ export const Page: React.FC<React.PropsWithChildren<PageProps>> = ({ title, chil
           </h2>
         ) : ( title )}
       </div>
-      <div className='grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-2'>
+      <div className='grid grid-cols-1 lg:grid-cols-[repeat(auto-fill,378px)] gap-2'>
         {children}
       </div>
     </div>
