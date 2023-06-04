f73e008014477d7bd9859c590b171643eb77792e
Ben Cochran <bcochran@gmail.com>
2023-06-04 01:29:10 -0700

    Expose subscriptions via GraphQL over SSE

--- apps/api/src/graphql/router.ts
+++ apps/api/src/graphql/router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { createYoga } from 'graphql-yoga'
 import { PrismaClient } from '@prisma/client';
+import { useGraphQLSSE } from '@graphql-yoga/plugin-graphql-sse';
 
 import { buildSchema } from './schema';
 
 export function buildRouter(prisma: PrismaClient, graphqlEndpoint: string) {
   const yoga = createYoga({
+    plugins: [useGraphQLSSE()],
     schema: buildSchema(prisma),
     graphqlEndpoint,
   });
